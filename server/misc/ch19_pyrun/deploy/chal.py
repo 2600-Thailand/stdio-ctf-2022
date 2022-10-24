5c8a4a078db5ac8b82c85fd86e7a82bec8337581
ErbaZZ <weerawat.paw@gmail.com>
2022-10-25 00:10:14 +0700

    Add Challenge 19 - PyRun

---
+++ server/misc/ch19_pyrun/deploy/chal.py
@@ -0,0 +1,21 @@
+blacklist = ["import", "upper", "lower", "open", "os", "system", "exit", "compile", "chr", "__import__", "object", "assert", "__builtins__", "exec", "eval", "_", "\\", "hex", "encode", "decode"]
+
+def check_blacklist(val):
+    for keyword in blacklist:
+        if keyword.lower() in val.lower():
+            print("The command specified is not allowed!")
+            return False
+
+    return True
+
+print("================= PyRun =================")
+print("Feel free to run your Python code here :)")
+
+while True:
+    val = input('$ ')
+    
+    if check_blacklist(val):
+        try:
+            exec(val)
+        except:
+            print("?")
