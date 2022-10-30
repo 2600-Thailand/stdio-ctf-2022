7347d54cf171d55ed9c001f263120fc92336ed7f
Bankde <bankde@hotmail.com>
2022-10-30 19:51:25 +0700

    ch14 - rename ch folder

--- file/run_ch14_m3_semi_sec_app.sh
+++ file/run_ch14_m3_semi_sec_app.sh
@@ -2,7 +2,7 @@
 # version 1
 
 export CATEGORY="web"
-export CHALLENGE_NAME="ch14_m3_xss2sqli"
+export CHALLENGE_NAME="ch14_m3_semi_sec_app"
 
 cd server/${CATEGORY}/${CHALLENGE_NAME}
 zip -r ../../../file/${CHALLENGE_NAME}.zip *
