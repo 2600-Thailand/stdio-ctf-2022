86d84337580de12e81f63cb067f52cb30aba0b41
Bankde <bankde@hotmail.com>
2022-11-04 16:50:21 +0700

    ch14 - refactor config

--- server/web/ch14_m3_semi_sec_app/instance/config.py
+++ server/web/ch14_m3_semi_sec_app/instance/config.py
@@ -1,15 +1,18 @@
 """Flask configuration."""
 import os
 
-def getEnv(env, fallback):
+def getEnv(env, fallback=None):
     if env in os.environ:
         return os.environ.get(env)
-    else:
+    elif fallback:
         return fallback
+    else:
+        # Forced fail, to prevent unintended leak secret/solution
+        raise Exception("Missing env: %s" % env)
 
-FLASK_DEBUG = getEnv("FLASK_DEBUG", "FALSE") # or TRUE
-SECRET_KEY = os.environ.get("SECRET_KEY") # or fail
-ADMIN_PASSWORD = getEnv("ADMIN_PASSWORD", "HelloWorld")
-CTF_FLAG = getEnv("CTF_FLAG", "FLAG{CHAIN_THEM_ALL}")
-CAPTCHA_SITE_KEY = getEnv("CAPTCHA_SITE_KEY", "")
-CAPTCHA_SECRET_KEY = getEnv("CAPTCHA_SECRET_KEY", "")
+FLASK_DEBUG = getEnv("FLASK_DEBUG", "FALSE") # FALSE/TRUE
+SECRET_KEY = getEnv("SECRET_KEY")
+ADMIN_PASSWORD = getEnv("ADMIN_PASSWORD")
+CTF_FLAG = getEnv("CTF_FLAG")
+CAPTCHA_SITE_KEY = getEnv("CAPTCHA_SITE_KEY")
+CAPTCHA_SECRET_KEY = getEnv("CAPTCHA_SECRET_KEY")
